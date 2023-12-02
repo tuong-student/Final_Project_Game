70fd3d6f492c52c0bdca17bccdef3b720727c569
Mia <>
2023-12-02 21:05:09 +0700

    push thiếu GameStatusSO

---
+++ Final_Project_Game/Assets/_Scripts/So/GameStatus.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/GameStatus")]
+public class GameStatus : ScriptableObject
+{
+    public bool isNewGame;
+}
