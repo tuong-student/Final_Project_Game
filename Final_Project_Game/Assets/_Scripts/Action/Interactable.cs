ed2d534b6035aa7e1f2c87aaa9b9c9eefff1fa79
NOOD_NTT <thanhtuongnguyen1@gmail.com>
2023-12-05 19:21:56 +0700

    fix mouse pointer

--- Final_Project_Game/Assets/_Scripts/Action/Interactable.cs
+++ Final_Project_Game/Assets/_Scripts/Action/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interactable : MonoBehaviour
+public abstract class Interactable : MonoBehaviour, IPointerInteractable
 {
     public abstract void Interact(Character character);
 }
