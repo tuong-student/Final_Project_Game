64d9a0c19a6f2ecbee7e58eaab1c3488cbca2b55
Mia <>
2023-12-24 19:20:15 +0700

    Fix bug

--- Final_Project_Game/Assets/_Scripts/UI/PanelGroup.cs
+++ Final_Project_Game/Assets/_Scripts/UI/PanelGroup.cs
@@ -16,14 +16,14 @@ public class PanelGroup : MonoBehaviorInstance<PanelGroup>
         inventoryPanel.SetActive(true);
         this.gameObject.transform.DOScale(1, 1f);
         _canvasGroup.DOFade(1, 0.7f);
-        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
     }
     public void Close()
     {
         this.gameObject.transform.DOScale(0.3f, 1f);
         _canvasGroup.DOFade(0, 0.7f).OnComplete(() =>
         {
-            _canvasGroup.interactable = false;
+            _canvasGroup.blocksRaycasts = false;
             inventoryPanel.gameObject.SetActive(false);
         });
     }
